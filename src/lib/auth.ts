5b933abfe7ab8e8be244c599653738a4a7311bb8
Rezzoug2003 <bilalrzog@gmail.com>
2025-01-26 22:45:30 +0100

    test git commend

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -35,12 +35,12 @@ export const authOption: AuthOptions = {
   ...authConfig,
   providers: [
     GitHubProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     CredentialsProvider({
       async authorize(credentials) {
